0df1c29b3bd396e5a325d9fb7dd5a23636cb3096
Ibrahim Abdulraheem <ibroraheem95@gmail.com>
2023-10-11 17:30:23 +0100

    init commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const axios = require('axios')
+
+class SudoAfrica{
+    constructor(apiKey, baseUrl){
+        this.apiKey = apiKey;
+        this.baseUrl = baseUrl;
+    }
+    as
+}
+
+module.exports = SudoAfrica
