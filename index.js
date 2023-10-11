7ec8a333df83435b2a375fa675729439631e49d1
Ibrahim Abdulraheem <ibroraheem95@gmail.com>
2023-10-11 21:32:26 +0100

    Completed customer routes

--- index.js
+++ index.js
@@ -1,11 +1,105 @@
 const axios = require('axios')
 
-class SudoAfrica{
-    constructor(apiKey, baseUrl){
+class SudoAfrica {
+    constructor(apiKey, baseUrl) {
         this.apiKey = apiKey;
         this.baseUrl = baseUrl;
     }
-    as
+    async createCustomer(data) {
+        const options = {
+            method: 'POST',
+            url: `${this.baseUrl}/customers`,
+            headers: {
+                accept: 'application/json',
+                'content-type': 'application/json',
+                Authorization: `Bearer ${this.apiToken}`
+            },
+            data
+        };
+
+        try {
+            const response = await axios.request(options);
+            return response.data;
+        } catch (error) {
+            throw new Error(`Failed to create customer: ${error.message}`);
+        }
+    }
+    async getCustomers(page = 0, limit = 100) {
+        const options = {
+            method: 'GET',
+            url: `${this.baseUrl}/customers?page=${page}&limit=${limit}`,
+            headers: {
+                accept: 'application/json',
+                Authorization: `Bearer ${this.apiToken}`
+            }
+        };
+
+        try {
+            const response = await axios.request(options);
+            return response.data;
+        } catch (error) {
+            throw new Error(`Failed to fetch customers: ${error.message}`);
+        }
+    }
+    async getCustomerById(customerId) {
+        const options = {
+            method: 'GET',
+            url: `${this.baseUrl}/customers/${customerId}`,
+            headers: {
+                accept: 'application/json',
+                Authorization: `Bearer ${this.apiToken}`
+            }
+        };
+
+        try {
+            const response = await axios.request(options);
+            return response.data;
+        } catch (error) {
+            throw new Error(`Failed to fetch customer: ${error.message}`);
+        }
+    }
+    async updateCustomer(customerId, customerData) {
+        const options = {
+            method: 'PUT',
+            url: `${this.baseUrl}/customers/${customerId}`,
+            headers: {
+                accept: 'application/json',
+                'content-type': 'application/json',
+                Authorization: `Bearer ${this.apiToken}`
+            },
+            data: customerData
+        };
+
+        try {
+            const response = await axios.request(options);
+            return response.data;
+        } catch (error) {
+            throw new Error(`Failed to update customer: ${error.message}`);
+        }
+    }
+    async updateCustomerDocumentUrl(customerId, fileName, fileType) {
+        const options = {
+            method: 'PUT',
+            url: `${this.baseUrl}/customers/${customerId}/documents/url`,
+            headers: {
+                accept: 'application/json',
+                'content-type': 'application/json',
+                Authorization: `Bearer ${this.apiToken}`
+            },
+            data: {
+                fileName: fileName,
+                fileType: fileType
+            }
+        };
+
+        try {
+            const response = await axios.request(options);
+            return response.data;
+        } catch (error) {
+            throw new Error(`Failed to update customer document URL: ${error.message}`);
+        }
+    }
+    
 }
 
 module.exports = SudoAfrica
